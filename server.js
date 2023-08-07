bd28fe4e7a8311ccd2ffe2f52c75da9111ab14e5
Aryanoo1 <96529828+Aryanoo1@users.noreply.github.com>
2023-08-08 03:20:05 +0530

    Revert "change commited once more"
    This reverts commit 47136e9de53e9320407846756931c1586abd199a.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 // import {express} from 'express'
 const app = express()
-const PORT = process.env.PORT
+const PORT = require('./config').PORT
 // import {PORT} from './config.js'
 const path = require('path')
 app.use(express.json())
