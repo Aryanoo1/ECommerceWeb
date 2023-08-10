48dc0ac4925923e9fd9233aef9b171d79c9ba25d
Aryanoo1 <96529828+Aryanoo1@users.noreply.github.com>
2023-08-10 21:13:06 +0530

    added

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const Sequelize = require('sequelize')
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
-const db = new Sequelize(urlDB,{
+// const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+const db = new Sequelize(process.env.DATABASE_URL,{
     dialect: 'mysql'
 })
-const port = process.env.MYSQLPORT
+// const port = process.env.MYSQLPORT
 exports = module.exports = {
-    db, Sequelize, port
+    db, Sequelize
 }
