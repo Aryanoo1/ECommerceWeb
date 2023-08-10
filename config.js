b08f20d6dc9906ce2fa9fd3f47f8d7b6df82893e
Aryanoo1 <96529828+Aryanoo1@users.noreply.github.com>
2023-08-10 18:07:22 +0530

    updated

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-const PORT = process.env.PORT || 1310
-const DB_USER = process.env.DB_USER || 'shopman'
-const DB_HOST = process.env.DB_HOST || 'localhost'
-const DB_PASSWORD = process.env.DB_PASSWORD || 'shoppass'
-const DB_NAME = process.env.DB_NAME || 'shopweb'
-exports = module.exports = {
-    DB_HOST,DB_NAME,DB_PASSWORD,DB_USER,PORT
-}
+require("dotenv").config();
+const Sequelize = require('sequelize')
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+const db = new Sequelize(urlDB,{
+    dialect: 'mysql'
+})
+module.exports = db;
